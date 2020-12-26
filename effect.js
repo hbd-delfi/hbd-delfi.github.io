cb688cdeda1831fd6e43e517d387168116ec83c6
hbd-delfi <vroooh@gmx.com>
2020-12-26 23:46:16 +0700

    edit effect.js

--- effect.js
+++ effect.js
@@ -7,13 +7,11 @@ $('document').ready(function(){
 		$(window).resize(function(){
 			 vw = $(window).width()/2;
 			$('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
-			$('#b11').animate({top:240, left: vw-350},500);
-			$('#b22').animate({top:240, left: vw-250},500);
-			$('#b33').animate({top:240, left: vw-150},500);
-			$('#b44').animate({top:240, left: vw-50},500);
-			$('#b55').animate({top:240, left: vw+50},500);
-			$('#b66').animate({top:240, left: vw+150},500);
-			$('#b77').animate({top:240, left: vw+250},500);
+			$('#b11').animate({top:240, left: vw-150},500);
+			$('#b22').animate({top:240, left: vw-75},500);
+			$('#b33').animate({top:240, left: vw-0},500);
+			$('#b44').animate({top:240, left: vw+75},500);
+			$('#b55').animate({top:240, left: vw+150},500);
 		});
 
 	$('#turn_on').click(function(){
@@ -148,10 +146,10 @@ $('document').ready(function(){
 		$('#b3').attr('id','b33')
 		$('#b4').attr('id','b44')
 		$('#b5').attr('id','b55')
-		$('#b11').animate({top:240, left: vw-250},500);
-		$('#b22').animate({top:240, left: vw-150},500);
-		$('#b33').animate({top:240, left: vw-50},500);
-		$('#b44').animate({top:240, left: vw+50},500);
+		$('#b11').animate({top:240, left: vw-150},500);
+		$('#b22').animate({top:240, left: vw-75},500);
+		$('#b33').animate({top:240, left: vw-0},500);
+		$('#b44').animate({top:240, left: vw+75},500);
 		$('#b55').animate({top:240, left: vw+150},500);
 		$('.balloons').css('opacity','0.9');
 		$('.balloons h2').fadeIn(3000);
